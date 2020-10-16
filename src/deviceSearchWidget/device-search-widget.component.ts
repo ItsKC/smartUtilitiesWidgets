deccc8249a7b0310115c3c095f5975a16561e84b
sasmazemre7 <sasmazemre7@gmail.com>
2020-10-16 11:08:36 +0300

    Add fiter, sort, paginator functions

--- src/deviceSearchWidget/device-search-widget.component.ts
+++ src/deviceSearchWidget/device-search-widget.component.ts
@@ -1,12 +1,54 @@
-import { Component, OnInit, OnDestroy, Input  } from '@angular/core';
+import { Component, ViewChild, OnInit, NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource} from '@angular/material/table';
+import { MatSort} from '@angular/material';
+import { MatPaginator} from '@angular/material';
+import { IHero } from './device-search.interfaces';
 
 @Component({
   selector: 'device-search-widget',
   templateUrl: './device-search-widget.html',
   styleUrls: ['../styles/bootstrap.min.css', './device-search-widget.component.css']
 })
-export class DeviceSearchWidget {
 
-    constructor() {
-	}
+
+export class DeviceSearchWidget implements OnInit {
+
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  displayedColumns = ['id', 'name', 'fullName', 'placeOfBirth', 'publisher'];
+  dataSource = new MatTableDataSource<IHero>();
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  ngOnInit() {
+    setTimeout(() => this.dataSource.paginator = this.paginator);
+    
+    this.dataSource.sort = this.sort;
+    this.httpClient.get('http://localhost:3000/heros')
+      .subscribe((heros: IHero[]) => {
+        this.dataSource.data = heros;
+      });
+  }
+
+  
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+myplaceHolder: string ='Search'
+
+ checkPlaceHolder() {
+    if (this.myplaceHolder) {
+      this.myplaceHolder = null
+      return;
+    } else {
+      this.myplaceHolder = 'Search'
+      return
+    }
+  }
 }
